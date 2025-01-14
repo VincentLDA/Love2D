2bf3b4acdaa3fcc18ca1073576365baee98a7190
VincentLDA <71265424+VincentLDA@users.noreply.github.com>
2025-01-14 23:57:26 +0100

    PongIpong

--- Pong/main.lua
+++ Pong/main.lua
@@ -1,3 +1,6 @@
+-- Cette ligne permet d'afficher des traces dans la console pendant l'éxécution
+io.stdout:setvbuf('no')
+
 playerSize = {}
 playerSize.height= 80 
 playerSize.width = 15 
@@ -7,23 +10,26 @@ balle.posX = 50
 balle.posY = 10
 balle.width = 20
 balle.height = 20
-balle.speedX = 2
-balle.speedY = 2
+balle.speedX = 1
+balle.speedY = 1
 
 rightPlayer = {}
 rightPlayer.height= playerSize.height 
 rightPlayer.width = playerSize.width 
 rightPlayer.posX = 40
 rightPlayer.posY = 10 
-rightPlayer.speed = 100 
+rightPlayer.speed = 20 
 
 leftPlayer = {}
 leftPlayer.height= playerSize.height 
 leftPlayer.width = playerSize.width 
 leftPlayer.posX = 10
 leftPlayer.posY = 10
-leftPlayer.speed = 100 
+leftPlayer.speed = 20 
  
+score = {}
+score.left = 0
+score.right = 0
 
 screen = {}
 screen.width = 0
@@ -41,20 +47,43 @@ function love.load()
     -- Calcule  position of ball    
     balle.posX =  (screen.width / 2) - (balle.width / 2)
     balle.posY = (screen.height / 2) - (balle.height / 2)
+    randomLaunchBall()
 
     -- Log
     de = "Screen W : "..screen.width.." H : "..screen.height
     print(de)
+
+    arrayX = {1,-1}
+    arrayY = {1,-1}
 end
 
 function love.update()
     movingPlayerLeft()
+    movingPlayerRight()
     moveBall()
 end
 
 function love.draw()
     initPlayer()
     initBall()
+    setScore()
+end
+
+function setScore()
+    textPrint = tostring(score.left).." : "..tostring(score.right)
+    font = love.graphics.getFont()
+
+    love.graphics.print(textPrint, screen.width/2 - font:getWidth(textPrint), 10)
+end
+function scoring(scoreLeft, scoreRight)
+    if scoreLeft == true then
+        score.left = score.left + 1  
+        resetBall()
+    elseif scoreRight == true then
+        score.right = score.right + 1  
+        resetBall()
+    end
+
 end
 
 
@@ -69,14 +98,30 @@ function movingPlayerLeft()
     if love.keyboard.isDown("z") then
         newPosY = leftPlayer.posY - 5 
         if newPosY > 0 then
-            leftPlayer.posY = leftPlayer.posY - 10        
+            leftPlayer.posY = leftPlayer.posY - 5        
         end
     end
 
     if love.keyboard.isDown("s") then
         newPosY = leftPlayer.posY + leftPlayer.height + 5 
         if newPosY < screen.height then
-            leftPlayer.posY = leftPlayer.posY + 10       
+            leftPlayer.posY = leftPlayer.posY + 5       
+        end
+    end
+
+end
+function movingPlayerRight()
+    if love.keyboard.isDown("up") then
+        newPosY = rightPlayer.posY - 5 
+        if newPosY > 0 then
+            rightPlayer.posY = rightPlayer.posY - 5        
+        end
+    end
+
+    if love.keyboard.isDown("down") then
+        newPosY = rightPlayer.posY + rightPlayer.height + 5 
+        if newPosY < screen.height then
+            rightPlayer.posY = rightPlayer.posY + 5       
         end
     end
 
@@ -85,20 +130,57 @@ end
 function initBall()
     love.graphics.rectangle("fill",balle.posX, balle.posY, balle.width,  balle.height)
 end
-function moveBall()
-    if balle.posY == 0 then
-        balle.speedY = 2
-    end
-    if (balle.posY + balle.height) == screen.height then
-        balle.speedY = -2
-    end
+function resetBall()
+    -- Calcule  position of ball    
+    balle.posX =  (screen.width / 2) - (balle.width / 2)
+    balle.posY = (screen.height / 2) - (balle.height / 2)
 
-    if balle.posX == 0 then
-        balle.speedX = 2
-    end
-    if (balle.posX + balle.width) == screen.width then
-        balle.speedX = -2
-    end
+    randomLaunchBall()    
+end
+function randomLaunchBall()
+    --Random launch
+    arrayX = {1,-1}
+    arrayY = {1,-1}
+    balle.speedX = arrayX[math.random(1,2)]
+    balle.speedY = arrayY[math.random(1,2)]
+end
+
+function moveBall()
+    if balle.posX <= leftPlayer.posX + leftPlayer.width and balle.posY + balle.height > leftPlayer.posY and balle.posY < leftPlayer.posY + leftPlayer.height then
+        --Up speed
+        balle.speedX = balle.speedX - 0.5
+        -- Switch direction
+        balle.speedX = -balle.speedX   
+
+        balle.posX = leftPlayer.posX + leftPlayer.width       
+    elseif balle.posX + balle.width >= rightPlayer.posX and balle.posY + balle.height > rightPlayer.posY and balle.posY < rightPlayer.posY + rightPlayer.height then
+        --Up speed
+        balle.speedX = balle.speedX + 0.5
+        -- Switch direction
+        balle.speedX = balle.speedX * -1   
+        balle.posX = rightPlayer.posX - balle.width  
+    else   
+        if balle.posY <= 0 then
+            --Set positif
+            balle.speedY =  -balle.speedY
+        end
+        if (balle.posY + balle.height) == screen.height then
+            --Set negatif
+            balle.speedY = balle.speedY * -1
+        end
+    
+        if balle.posX <= 0 then
+            --Set positif
+            balle.speedX = -balle.speedX
+            scoring(false,true)
+        end
+        if (balle.posX + balle.width) >= screen.width then
+            --Set negatif
+            balle.speedX = balle.speedX * -1
+            
+            scoring(true,false)
+        end
+    end 
 
 
     balle.posX = balle.posX + balle.speedX 
